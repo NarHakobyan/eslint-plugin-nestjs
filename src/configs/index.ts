650c370f6d63031c37851aed210fa869e19a47a4
Narek <narek.hakobyan.07@gmail.com>
2022-04-18 12:49:27 +0400

    feat(app): release 1.1.0

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,9 +1,11 @@
-import noSwagger from './noSwagger';
+import noSwagger from './no-swagger';
 import recommended from './recommended';
 
 const allConfigs = {
   recommended,
+  // eslint-disable-next-line @typescript-eslint/naming-convention
   'no-swagger': noSwagger,
 };
 
+// eslint-disable-next-line import/no-default-export
 export default allConfigs;
---
+++ src/configs/no-swagger.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+export = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: { sourceType: 'module' },
+  rules: {
+    '@moneteam/nestjs/api-property-matches-property-optionality': 'off',
+    '@moneteam/nestjs/api-method-should-specify-api-response': 'off',
+    // '@moneteam/nestjs/controllers-should-supply-api-tags': 'off',
+    '@moneteam/nestjs/api-enum-property-best-practices': 'off',
+    '@moneteam/nestjs/api-property-returning-array-should-set-array': 'off',
+  },
+};
